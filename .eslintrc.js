da1ec52d95a873e28c54d32ba555a0dbbbd2989d
Marcus Balbi <balbimarcus@gmail.com>
2020-09-01 21:34:04 -0300

    project configured

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,33 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: "babel-eslint",
   },
   rules: {
+    "prettier/prettier": [
+        "warn",
+        {
+          "doubleQuote": true,
+          "semi": true,
+          "trailingComma": "all"
+        }
+    ],
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
   overrides: [
     {
       files: [
         "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
       ],
       env: {
-        jest: true
-      }
-    }
-  ]
+        jest: true,
+      },
+    },
+  ],
 };
